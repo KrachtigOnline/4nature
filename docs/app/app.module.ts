4067ff9aaf6881eb6a3ca96b7aaf1afd615aa0a6
Robin Ceustermans <roobiinboy@gmail.com>
2022-06-09 16:00:08 +0200

    docs

--- docs/app/app.module.ts
+++ docs/app/app.module.ts
@@ -18,7 +18,7 @@ import { CookieComponent } from './footer/cookie/cookie.component';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import {UtilityService} from './utility.service';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+//import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 // @ts-ignore
 // @ts-ignore
@@ -48,9 +48,12 @@ import {LocationStrategy, HashLocationStrategy} from '@angular/common';
   exports: [
     ReactiveFormsModule
   ],
-  providers: [UtilityService,
-    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  providers: [UtilityService
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+// {provide: LocationStrategy, useClass: HashLocationStrategy}
